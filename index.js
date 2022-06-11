4659c9ce9d516cb7ee406213ab20bd65b419ebdd
courtlandcampo <courtland.campo@ufl.edu>
2022-06-11 13:52:36 -0400

    Remove li if clicked twice

--- index.js
+++ index.js
@@ -15,19 +15,27 @@ var form = document.querySelector('form')
 
 
 form.onsubmit = function(e) {
-    e.preventDefault()
-    var item = document.querySelector('input')
+    e.preventDefault() //stops HTML from taking over
+    var item = document.querySelector('input') //accessing form inputs
     
     var toDoListEl = document.querySelector('#todo-list')
-    var liEl = document.createElement('li')
-    var btnEl = document.createElement('button')
-    toDoListEl.appendChild(liEl)
+    var liEl = document.createElement('li') // creates bullet points
+    var btnEl = document.createElement('button') //makes bullet points buttons
+    toDoListEl.appendChild(liEl) //add bullets to the to do list
     liEl.appendChild(btnEl)
-    btnEl.textContent = item.value
-    item.value = ""
-
+    
+    btnEl.textContent = item.value //inserts form string into the button
+    
+    item.value = "" //clears form field
+    
+    var buttonClicks = 0 // track how many times a button is clicked
     btnEl.onclick = function() {
-        btnEl.style.textDecoration = 'line-through'
+        if (buttonClicks === 0){
+        btnEl.style.textDecoration = 'line-through' //one click = strikthrough
+        buttonClicks++
+        } else if (buttonClicks ===1){ 
+            liEl.remove() //two clicks = removal
+        }
     }
 }
 
